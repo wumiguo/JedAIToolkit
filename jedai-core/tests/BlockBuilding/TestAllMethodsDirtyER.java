627a7440bc318d44207dbbc2790f0eb2e52301f0
gpapadis <gpapadis@yahoo.gr>
2017-05-12 11:06:16 +0300

    Updated examples.

--- jedai-core/tests/BlockBuilding/TestAllMethodsDirtyER.java
+++ jedai-core/tests/BlockBuilding/TestAllMethodsDirtyER.java
@@ -33,7 +33,7 @@ import java.util.List;
  * @author G.A.P. II
  */
 
-public class TestAllMethods {
+public class TestAllMethodsDirtyER {
     public static void main(String[] args) {
         String entitiesFilePath = "C:\\Users\\G.A.P. II\\Downloads\\cddbProfiles";
         String groundTruthFilePath = "C:\\Users\\G.A.P. II\\Downloads\\cddbDuplicates";
