d47d2ace34f810f4432f339bbcb5892b4fecd742
gpapadis <gpapadis@yahoo.gr>
2017-12-21 11:39:06 +0200

    Minor fixes.

--- jedai-core/src/test/java/EntityMatching/TestGroupLinkage.java
+++ jedai-core/src/test/java/EntityMatching/TestGroupLinkage.java
@@ -12,8 +12,7 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
-*/
-
+ */
 package EntityMatching;
 
 import BlockBuilding.*;
@@ -37,17 +36,17 @@ import java.util.List;
  *
  * @author G.A.P. II
  */
-
 public class TestGroupLinkage {
+
     public static void main(String[] args) {
-    	String entitiesFilePath1 = "/home/ethanos/Downloads/JEDAIfiles/im-identity/oaei2014_identity_aPROFILES";
-    	//String entitiesFilePath2 = "/home/ethanos/Downloads/JEDAIfiles/im-identity/oaei2014_identity_aPROFILES";//
+        String entitiesFilePath1 = "/home/ethanos/Downloads/JEDAIfiles/im-identity/oaei2014_identity_aPROFILES";
+        //String entitiesFilePath2 = "/home/ethanos/Downloads/JEDAIfiles/im-identity/oaei2014_identity_aPROFILES";//
         String groundTruthFilePath = "/home/ethanos/Downloads/JEDAIfiles/cddbTestDuplicates";
-        
+
         IEntityReader eReader1 = new EntitySerializationReader(entitiesFilePath1);
         List<EntityProfile> profiles1 = eReader1.getEntityProfiles();
         System.out.println("Input Entity Profiles\t:\t" + profiles1.size());
-        
+
 //        IEntityReader eReader2 = new EntitySerializationReader(entitiesFilePath2);
 //        List<EntityProfile> profiles2 = eReader2.getEntityProfiles();
 //        System.out.println("Input Entity Profiles\t:\t" + profiles2.size());
@@ -55,26 +54,23 @@ public class TestGroupLinkage {
         IGroundTruthReader gtReader = new GtSerializationReader(groundTruthFilePath);
         final AbstractDuplicatePropagation duplicatePropagation = new UnilateralDuplicatePropagation(gtReader.getDuplicatePairs(eReader1.getEntityProfiles()));
         System.out.println("Existing Duplicates\t:\t" + duplicatePropagation.getDuplicates().size());
-        
-        for (BlockBuildingMethod blbuMethod : BlockBuildingMethod.values()) {
-        	if (blbuMethod.equals(BlockBuildingMethod.ATTRIBUTE_CLUSTERING))
-        	{
 
+        for (BlockBuildingMethod blbuMethod : BlockBuildingMethod.values()) {
             System.out.println("\n\nCurrent blocking metohd\t:\t" + blbuMethod);
             IBlockBuilding blockBuildingMethod = BlockBuildingMethod.getDefaultConfiguration(blbuMethod);
             List<AbstractBlock> blocks = blockBuildingMethod.getBlocks(profiles1);//
             System.out.println("Original blocks\t:\t" + blocks.size());
-            
+
             IBlockProcessing blockCleaningMethod = BlockBuildingMethod.getDefaultBlockCleaning(blbuMethod);
             if (blockCleaningMethod != null) {
                 blocks = blockCleaningMethod.refineBlocks(blocks);
             }
-            
+
             IBlockProcessing comparisonCleaningMethod = BlockBuildingMethod.getDefaultComparisonCleaning(blbuMethod);
             if (comparisonCleaningMethod != null) {
                 blocks = comparisonCleaningMethod.refineBlocks(blocks);
             }
-            
+
 //            for ( AbstractBlock bl : blocks) {
 //            	System.out.println("new");
 //            	ComparisonIterator iterator = bl.getComparisonIterator();
@@ -83,29 +79,23 @@ public class TestGroupLinkage {
 //                	System.out.println(currentComparison.getEntityId1()+" "+currentComparison.getEntityId2());
 //                }
 //            }
-
-
-   
-
-            long start = System.nanoTime(); 
+            long start = System.nanoTime();
             for (RepresentationModel model : RepresentationModel.values()) {
-            	if (model.equals(RepresentationModel.CHARACTER_BIGRAMS))
-            	{
+                if (model.equals(RepresentationModel.CHARACTER_BIGRAMS)) {
 
-                GroupLinkage gp = new GroupLinkage(0.1, model, SimilarityMetric.getModelDefaultSimMetric(model));
-                gp.setSimilarityThreshold(0.1);
-                SimilarityPairs simPairs = gp.executeComparisons(blocks, profiles1);//
-                //System.out.println();
-                for (int i = 0; i < simPairs.getNoOfComparisons(); i++) {
-                    //System.out.println(simPairs.getEntityIds1()[i] + "\t\t" + simPairs.getEntityIds2()[i] + "\t\t" + simPairs.getSimilarities()[i]);
+                    GroupLinkage gp = new GroupLinkage(0.1, model, SimilarityMetric.getModelDefaultSimMetric(model));
+                    gp.setSimilarityThreshold(0.1);
+                    SimilarityPairs simPairs = gp.executeComparisons(blocks, profiles1);//
+                    //System.out.println();
+                    for (int i = 0; i < simPairs.getNoOfComparisons(); i++) {
+                        //System.out.println(simPairs.getEntityIds1()[i] + "\t\t" + simPairs.getEntityIds2()[i] + "\t\t" + simPairs.getSimilarities()[i]);
+                    }
                 }
-            	}
-            	
+
+                long elapsedTime = System.nanoTime() - start;
+                System.out.println("time=" + elapsedTime / 1000000000.0);
             }
-            long elapsedTime = System.nanoTime() - start;
-			System.out.println("time="+elapsedTime/1000000000.0);
-        	}
-        	
+
         }
     }
 }
