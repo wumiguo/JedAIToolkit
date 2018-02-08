921845336a54f4aa2d21b6ec3c4ed38c89c9888c
mthanos <manthan16@hotmail.com>
2018-02-08 09:58:09 +0100

    Update Test for csv to RDF converter

--- jedai-core/src/test/java/DataReader/TestEntityCSVReader.java
+++ jedai-core/src/test/java/DataReader/TestEntityCSVReader.java
@@ -12,19 +12,21 @@ import java.util.List;
 
 public class TestEntityCSVReader {
     public static void main(String[] args) {
-        String filePath = "C:\\Users\\G.A.P. II\\Downloads\\cd.csv";
+        String filePath = "C:/Users/Manos/workspaceMars/JedAIgitFinal/JedAIgitFinal/datasets/converter/DBLP2.csv";
         EntityCSVReader csvReader = new EntityCSVReader(filePath);
         csvReader.setAttributeNamesInFirstRow(true);
         csvReader.setSeparator(';');
         csvReader.setAttributesToExclude(new int[]{1});
         csvReader.setIdIndex(0);
         List<EntityProfile> profiles = csvReader.getEntityProfiles();
-//        for (EntityProfile profile : profiles) {
-//            System.out.println("\n\n" + profile.getEntityUrl());
-//            for (Attribute attribute : profile.getAttributes()) {
-//                System.out.println(attribute.toString());
-//            }
-//        }
-        csvReader.storeSerializedObject(profiles, "C:\\Users\\G.A.P. II\\Downloads\\cddbProfiles");
+        for (EntityProfile profile : profiles) {
+            System.out.println("\n\n" + profile.getEntityUrl());
+            for (Attribute attribute : profile.getAttributes()) {
+                System.out.println(attribute.toString());
+            }
+        }
+        csvReader.storeSerializedObject(profiles, "C:/Users/Manos/workspaceMars/JedAIgitFinal/JedAIgitFinal/datasets/converter/DBLP2prof");
+        csvReader.convertToRDFfile(profiles, "C:/Users/Manos/workspaceMars/JedAIgitFinal/JedAIgitFinal/datasets/converter/DBLP2toRDFxml.xml");
+
     }
 }
