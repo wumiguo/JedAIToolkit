8a35d883dd9f3dfb13af10d644cccd7aca6ed76e
Lee Ween Jiann <lwj5@hotmail.com>
2020-03-28 17:11:41 +0800

    Fix numeric precision

--- src/main/java/org/scify/jedai/progressivejoin/JaccardTopK.java
+++ src/main/java/org/scify/jedai/progressivejoin/JaccardTopK.java
@@ -1,17 +1,17 @@
 /*
-* Copyright [2016-2020] [George Papadakis (gpapadis@yahoo.gr)]
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
+ * Copyright [2016-2020] [George Papadakis (gpapadis@yahoo.gr)]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.scify.jedai.progressivejoin;
 
@@ -74,7 +74,7 @@ public class JaccardTopK {
     }
 
     public double upperbound_access_internal(int probe, int ind) {
-        return probe * ind / (probe + ind - probe * ind);
+        return (double) probe * ind / (probe + ind - probe * ind);
     }
 
     public double upperbound_index(int len1, int pos) {
